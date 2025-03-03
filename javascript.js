82c5fe8ba9ea25d57def16cae21fefdce0b9be74
jim-ubuntu <jimferguson_3@protonmail.com>
2025-03-03 23:22:47 +0000

    Created boilerplate and began javascript with functions.

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function addition(addend1, addend2){
+    return addend1 + addend2;
+}
+
+function subtraction(minuend, subtrahend){
+    return minuend - subtrahend;
+}
+
+function division(dividend, divisor){
+    return dividend / divisor;
+}
+
+function multiplication(factor1, factor2){
+    return factor1 * factor2;
+}
+
+const calculatorContainer = document.createElement("div");
+calculatorContainer.textContent = "Hey there...";
+document.body.appendChild(calculatorBody);
+//function createCalculator(){
+//}
+//Needs display, buttons, etc..... 
