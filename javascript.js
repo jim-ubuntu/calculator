05b23487079caafb915c6ca140eea0ab15d666ee
jim-ubuntu <jimferguson_3@protonmail.com>
2025-03-05 18:12:34 +0000

    Implemented the layout and so on of the calculator. Still working on the logic.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,37 @@
+let isFirstOperation = false;
+let isFirstNumOfSecondNum = false;
+let operatorSelected = false;
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+let newDisplay = "";
+
+const display = document.getElementById("calc-display");
+const numberButtons = document.querySelectorAll(".number-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+operatorButtons.forEach((item) =>{
+    item.addEventListener("click", () =>{
+        operatorSelected = true;
+    })
+})
+
+// numberButtons.forEach((item) => {})
+
+numberButtons.forEach((item) =>{
+    item.addEventListener("click", () =>{
+        if (operatorSelected == true){
+            display.textContent = display.textContent.substring(display.textContent.length, display.textContent.length -1);
+            operatorSelected = false
+        // if (isFirstNumOfSecondNum == true){
+        //     newDisplay = display.textContent.substring(display.textContent.length, display.textContent.length -1);
+        //     display.textContent = newDisplay;
+        //     return isFirstNumOfSecondNum = true;
+            
+        // }
+        }
+    })
+})
+
 function addition(addend1, addend2){
     return addend1 + addend2;
 }
@@ -14,9 +48,31 @@ function multiplication(factor1, factor2){
     return factor1 * factor2;
 }
 
-const calculatorContainer = document.createElement("div");
-calculatorContainer.textContent = "Hey there...";
-document.body.appendChild(calculatorBody);
-//function createCalculator(){
-//}
-//Needs display, buttons, etc..... 
+function operate(number1, operator ,number2){
+    if (operator == "+"){
+        return addition(number1, number2);
+        isFirstOperation = true;
+    }
+    else if (operator == "-"){
+        isFirstOperation = true;
+    }
+    else if (operator == "*"){
+        return multiplication(number1, number2);
+        isFirstOperation = true;
+    }
+    else if (operator == "/"){
+        return division(number1, number2);
+        isFirstOperation = true;
+    }
+}
+
+function getFirstNumber(){
+    operatorAlreadySelected = true;
+    return firstNumber = Number(display.textContent);
+    
+}
+
+function getSecondNumber(){
+    return secondNumber = Number(display.textContent);
+     
+}
